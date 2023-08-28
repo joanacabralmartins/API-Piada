f19e3bb82304cc79acdb760143afa76d3f0584e5
Joana Cabral <joanacabralmartins@outlook.com>
2023-08-28 20:59:29 -0300

    funcao para carregar imagens da api

--- api.js
+++ api.js
@@ -17,10 +17,35 @@ function loadJoke() {
             console.error('Ocorreu um erro ao carregar a piada:', error);
             jokeContainer.innerHTML = '<p>Erro ao carregar piada.</p>';
         });
+
+    const loadReactionButton = document.getElementById('loadReaction');
+    loadReactionButton.style.display = 'block';
+}
+
+function loadReaction() {
+    const reactionContainer = document.getElementById('reactionContainer');
+
+    axios.get('https://dog.ceo/api/breeds/image/random')
+        .then(response => {
+            const imageUrl = response.data.message;
+            const dogImage = document.createElement('img');
+            dogImage.src = imageUrl;
+            reactionContainer.innerHTML = '';
+            reactionContainer.appendChild(dogImage);
+        })
+        .catch(error => {
+            console.error('Ocorreu um erro ao carregar a imagem:', error);
+            reactionContainer.innerHTML = '<p>Ocorreu um erro ao carregar a imagem.</p>';
+        });
 }
 
 const loadJokeButton = document.getElementById('loadJoke'); 
+const loadReactionButton = document.getElementById('loadReaction');
     
 loadJokeButton.addEventListener('click', () => {
     loadJoke(); 
+});
+
+loadReactionButton.addEventListener('click', () => {
+    loadReaction(); 
 });
